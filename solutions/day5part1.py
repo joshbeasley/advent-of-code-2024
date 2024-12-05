8bb3c837a5f0f09cef84926e57bf30692caf9dd6
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-12-05 10:29:06 -0800

    day 5

---
+++ solutions/day5part1.py
@@ -0,0 +1,28 @@
+import re
+from collections import defaultdict
+
+def is_valid(update):
+  for i in range(len(update)-1):
+    for j in range(i+1, len(update)):
+      if update[j] in adj[update[i]]:
+        return False
+  return True
+
+res = 0
+text =  open(0).read()
+rules = re.findall(r"(\d+)\|(\d+)", text)
+rules = [(int(rule[0]), int(rule[1])) for rule in rules]
+
+updates = re.findall(r"((\d+,)+\d+)", text)
+updates = [list(map(int, i[0].split(','))) for i in updates]
+
+adj = defaultdict(list)
+
+for rule in rules:
+  adj[rule[1]].append(rule[0])
+
+for update in updates:
+  if is_valid(update):
+    res += update[len(update) // 2]
+
+print(res)
