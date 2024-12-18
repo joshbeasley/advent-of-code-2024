4152e5082f69a8aeae28f82c2622be4cf53b65c1
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-12-17 22:42:47 -0800

    day 13 part 1

---
+++ 2024/day13part1.py
@@ -0,0 +1,13 @@
+import re
+
+res = 0
+for line in open(0).read().split('\n\n'):
+  ax, ay, bx, by, px, py = map(int, re.findall(r"\d+", line))
+  sol_a = (px * by - py * bx) / (ax * by - ay * bx)
+  sol_b = (px - ax * sol_a) / bx
+  if sol_a % 1 == 0 and sol_b % 1 == 0:
+    res += int(3 * sol_a + sol_b)
+
+print(res)
+  
+    
