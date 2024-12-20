d114824fe40616b34d1c83dcd82d205a7d928ce0
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-12-19 20:55:09 -0800

    WIP day 15 part 2

--- 2024/day15part2.py
+++ 2024/day15part2.py
@@ -1,7 +1,23 @@
+
 res = 0
 grid, moves =  open(0).read().split('\n\n')
 grid = [list(line.strip()) for line in grid.split('\n')]
+replacement = {'#': '##', 'O': '[]', '.': '..', '@': '@.'}
 R, C = len(grid), len(grid[0])
+
+for r in range(R):
+  for c in range(C):
+    grid[r][c] = replacement[grid[r][c]]
+
+for r in range(R):
+  grid[r] = list(''.join(grid[r]))
+
+R, C = len(grid), len(grid[0])
+
+def print_grid():
+  for row in grid:
+    print(''.join(row))
+
 moves = list(''.join(moves.split('\n')))
 robot = None
 move_map = {'^': (-1, 0), 'v': (1, 0), '<': (0, -1), '>': (0, 1)}
@@ -10,10 +26,6 @@ for r in range(R):
   for c in range(C):
     if grid[r][c] == '@':
       robot = (r, c)
-
-def print_grid():
-  for row in grid:
-    print(''.join(row))
 
 def in_bounds(r, c):
   return r >= 0 and r < R and c >= 0 and c < C
@@ -25,24 +37,26 @@ def is_empty(r, c):
   return True if grid[r][c] == '.' else False
 
 def move_box(r, c, dr, dc):
-  should_print = False
-  if (r, c) == (1, 3):
-    should_print = True
-    print('hit')
-  nr, nc = r + dr, c + dc
-  num_blocks = 0
-  while in_bounds(nr, nc) and grid[nr][nc] == 'O':
-    num_blocks += 1
-    nr, nc = nr + dr, nc + dc
-  
-  if not in_bounds(nr, nc) or grid[nr][nc] == '#':
-    return (r, c)
-  
-  for i in range(num_blocks):
-    grid[nr][nc] = 'O'
-    nr, nc = nr - dr, nc - dc
-  
-  return (nr, nc)
+  if dr == 0:
+    nr, nc = r + dr, c + dc
+    num_blocks = 0
+    while in_bounds(nr, nc) and grid[nr][nc] == '[' or grid[nr][nc] == ']':
+      num_blocks += 1
+      nr, nc = nr + dr, nc + dc
+    
+    if not in_bounds(nr, nc) or grid[nr][nc] == '#':
+      return (r, c)
+    
+    for i in range(num_blocks):
+      if dc > 0:
+        grid[nr][nc] = '['
+        grid[nr - dr][nc - dc] = ']'
+      else:
+        grid[nr][nc] = ']'
+        grid[nr - dr][nc - dc] = '['
+      nr, nc = nr - 2 * dr, nc - 2 * dc
+    
+    return (nr, nc)
   
 
 def execute_move(robot, move):
@@ -61,9 +75,9 @@ for move in moves:
   old_pos = robot
   robot = execute_move(robot, move)
   grid[old_pos[0]][old_pos[1]] = '.'
+  print(robot[0], robot[1])
   grid[robot[0]][robot[1]] = '@'
   print_grid()
-  print()
 
 res = 0
 for r in range(R):
