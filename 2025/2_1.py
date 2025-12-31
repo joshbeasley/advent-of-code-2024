2fdb7a38bc5988a64a93a22faab6ceb01a644fbb
Josh Beasley <josh.beasley@hotmail.com>
2025-12-31 18:55:42 -0500

    day 2 part 1

---
+++ 2025/2_1.py
@@ -0,0 +1,15 @@
+ranges = open(0).read().split(',')
+output = 0
+
+for item in ranges:
+    first, last = item.split('-')
+    first, last = int(first), int(last)
+    for i in range(first, last + 1):
+        num = str(i)
+        num_len = len(num)
+        if num_len % 2 == 0 and num[:num_len // 2] == num[num_len // 2:]:
+            output += int(num)
+
+print(output)
+
+
